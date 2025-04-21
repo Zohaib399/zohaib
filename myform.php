db5ca92eceab6203b8aed7fa567147a453b1ce85
Zohaib399 <zohaibdeveloper513@gmail.com>
2025-04-21 18:53:37 +0500

    Add files via upload

---
+++ myform.php
@@ -0,0 +1,12 @@
+<$php>
+
+$fname = $_POST['fname'];
+$lastName = $_POST['lastName'];
+$fatherName = $_POST['fatherName'];
+$email = $_POST['email'];
+$phoneNumber = $_POST['phoneNumber'];
+$password = $_POST['password'];
+
+
+
+??>
